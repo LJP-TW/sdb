7c76b264e80792fbf6002ff03d456c027808023e
LJP-TW <accr94238@gmail.com>
2022-05-30 07:11:00 -0700

    Remove include stdarg.h

--- include/sdbcmd/help.h
+++ include/sdbcmd/help.h
@@ -1,8 +1,6 @@
 #ifndef _SDBCMD_HELP_H
 #define _SDBCMD_HELP_H
 
-#include <stdarg.h>
-
 int sdb_cmd_help(int argc, char **argv);
 
 #endif /* _SDBCMD_HELP_H */
--- include/sdbcmd/load.h
+++ include/sdbcmd/load.h
@@ -1,8 +1,6 @@
 #ifndef _SDBCMD_LOAD_H
 #define _SDBCMD_LOAD_H
 
-#include <stdarg.h>
-
 int sdb_cmd_load(int argc, char **argv);
 
 #endif /* _SDBCMD_LOAD_H */
