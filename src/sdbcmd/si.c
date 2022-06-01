0367e08e7f4db672c6c2fedb0bc69d72ee6ba94a
LJP-TW <accr94238@gmail.com>
2022-06-01 07:05:58 -0700

    Fix wrong logic of command si

--- src/sdbcmd/si.c
+++ src/sdbcmd/si.c
@@ -12,37 +12,13 @@ static inline void do_step_ins(void)
 {
     sdb_breakpoint_meta *bp;
     struct user_regs_struct regs;
-    uint64 data;
     int wstatus;
-
-    sdb.running = 1;
-
-    sdb_set_all_bp();
-
-    ptrace(PTRACE_GETREGS, sdb.pid, NULL, &regs);
-    data = ptrace(PTRACE_PEEKTEXT, sdb.pid, (void *)regs.rip, 0);
-
-    if (data & 0xff != 0xcc) {
-        goto DO_STEP_INS_END;
-    }
-
-    bp = sdb_find_bp(regs.rip, NULL);
-
-    if (!bp) {
-        goto DO_STEP_INS_END;
-    }
-
-    // Hit breakpoint again
-
-    sdb_resume_all_bp();
 
     sdb_unset_handler();
 
     ptrace(PTRACE_SINGLESTEP, sdb.pid, 0, 0);
 
     waitpid(sdb.pid, &wstatus, 0);
-
-    sdb.running = 0;
 
     if (WIFEXITED(wstatus)) {
         printf("** child process %d terminiated normally (code %d)\n",
@@ -53,17 +29,16 @@ static inline void do_step_ins(void)
         return;
     }
 
-    sdb_set_handler();
-
     ptrace(PTRACE_GETREGS, sdb.pid, NULL, &regs);
 
-    printf("** breakpoint @");
-    sdb_show_disasm(regs.rip);
+    bp = sdb_find_bp(regs.rip, NULL);
 
-    return;
+    if (bp) {
+        printf("** breakpoint @");
+        sdb_show_disasm(regs.rip);
+    }
 
-DO_STEP_INS_END:
-    ptrace(PTRACE_SINGLESTEP, sdb.pid, 0, 0);
+    sdb_set_handler();
 }
 
 int sdb_cmd_si(int argc, char **argv)
